f92dbfb7b0e9741dcfc28d9bbd198319fa073572
lazeez <qudratov.com26@gmail.com>
2025-04-12 21:09:17 +0500

    initial

---
+++ app/onboarding.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+// import { Link } from 'expo-router'
+
+const onboarding = () => {
+    return (
+        <View>
+            <Text>onboarding</Text>
+        </View>
+    )
+}
+
+export default onboarding
+
+const styles = StyleSheet.create({})
