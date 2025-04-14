047e60439940de3743d6265e3096287d846fdec8
lazeez <qudratov.com26@gmail.com>
2025-04-14 17:43:34 +0500

    Home page half

---
+++ components/Popular.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Popular = () => {
+    return (
+        <View className='mt-[50px]'>
+            <Text className='text-white font-bold text-[18px]'>Popular movies</Text>
+        </View>
+    )
+}
+
+export default Popular
