0b6ba4790e62d465cfd64d8d84d09032fd3fb89b
lazeez <qudratov.com26@gmail.com>
2025-04-19 20:19:49 +0500

    appwrite setup (sending data)

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import { fetchMovies } from '@/services/api';
 import MovieCard from '@/components/MovieCard';
 import { icons } from '@/constants/icons';
 import SearchBar from '@/components/SearchBar';
+import { updateSearchCount } from '@/services/appwrite';
 
 const Search = () => {
     const [searchQuery, setSearchQuery] = useState('')
@@ -16,6 +17,9 @@ const Search = () => {
         const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
+                if (movies?.length > 0 && movies?.[0]) {
+                    await updateSearchCount(searchQuery, movies[0]);
+                }
             } else {
                 reset()
             }
